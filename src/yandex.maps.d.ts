91466539c7a7286724072fd846cbac25e49880c5
Quang Ha Tran <quang-ha.tran@etu.unilim.fr>
2025-04-25 03:11:11 +0200

    Modify the type definitions to more properly match the api functions

--- src/yandex.maps.d.ts
+++ src/yandex.maps.d.ts
@@ -1,96 +1,92 @@
 declare namespace ymaps {
+    function ready(callback: () => void): Promise<void>;
+  
     class Map {
-        constructor(container: string | HTMLElement, options?: MapOptions);
-        setCenter(center: number[], zoom?: number, options?: any): Promise<void>;
-        getCenter(): number[];
+      constructor(element: HTMLElement | string, options: MapOptions);
+      events: EventManager;
+      geoObjects: GeoObjectCollection;
+      setCenter(center: number[], zoom?: number, options?: any): void;
+      getCenter(): number[];
+      getZoom(): number;
+      setType(type: string): void;
+      getType(): string;
+      destroy(): void;
+      behaviors: any;
+    }
+  
+    interface MapOptions {
+      center: number[];
+      zoom: number;
+      controls?: string[];
+      type?: string;
+    }
+  
+    class GeoObjectCollection {
+      add(object: any): this;
+      remove(object: any): this;
+      getLength(): number;
+      get(index: number): any;
+      getAll(): any[];
+      removeAll(): this;
+    }
+  
+    class Placemark {
+      constructor(geometry: number[], properties?: PlacemarkProperties, options?: PlacemarkOptions);
+      geometry: { 
+        getCoordinates(): number[]; 
+        setCoordinates(coords: number[]): void; 
+      };
+      properties: any;
+      options: any;
+      events: EventManager;
+    }
+  
+    interface PlacemarkProperties {
+      hintContent?: string;
+      balloonContent?: string;
+      iconContent?: string;
+      [key: string]: any;
+    }
+  
+    interface PlacemarkOptions {
+      preset?: string;
+      iconLayout?: string;
+      iconImageHref?: string;
+      iconImageSize?: number[];
+      iconImageOffset?: number[];
+      draggable?: boolean;
+      visible?: boolean;
+      [key: string]: any;
+    }
+  
+    interface EventManager {
+      add(type: string, callback: (e: any) => void): this;
+      remove(type: string, callback: (e: any) => void): this;
+      fire(type: string, event?: any): this;
+    }
+  
+    namespace panorama {
+      function locate(point: number[]): Promise<any[]>;
+      function createPlayer(element: string | HTMLElement, point: number[], options?: PlayerOptions): Promise<Player>;
+      function isSupported(): boolean;
+  
+      class Player {
+        constructor(element: string | HTMLElement, panorama: any, options?: PlayerOptions);
+        getPanorama(): any;
+        setPanorama(panorama: any): Promise<void>;
+        setDirection(direction: number[] | string): void;
+        getDirection(): number[];
         setZoom(zoom: number): void;
         getZoom(): number;
-        setType(type: string | MapType): void;
-        getType(): string;
-        events: IEventManager;
-        controls: any;
-        geoObjects: any;
+        events: EventManager;
         destroy(): void;
-    }
-
-    interface MapOptions {
-        center?: number[];
+      }
+  
+      interface PlayerOptions {
+        direction?: number[] | string;
+        controls?: string[];
         zoom?: number;
-        type?: string | MapType;
-        controls?: string[];
-        behaviors?: string[];
+        [key: string]: any;
+      }
     }
-
-    class MapType {
-        constructor(name: string, options: any);
-    }
-
-    class Placemark {
-        constructor(geometry: number[], properties?: any, options?: any);
-        events: IEventManager;
-        geometry: IGeometry;
-        options: IOptionManager;
-        properties: IDataManager;
-        setMap(map: Map | null): void;
-    }
-
-    class SuggestView {
-        constructor(element: string | HTMLElement, options?: any);
-        events: IEventManager;
-    }
-
-    interface IEventManager {
-        add(types: string | string[], callback: Function, context?: any, priority?: number): this;
-        remove(types: string | string[], callback: Function, context?: any, priority?: number): this;
-        fire(type: string, event?: object | IEvent): this;
-    }
-
-    interface IEvent {
-        originalEvent: any;
-        get(name: string): any;
-    }
-
-    interface IGeometry {
-        getCoordinates(): number[];
-        setCoordinates(coordinates: number[]): void;
-    }
-
-    interface IOptionManager {
-        set(key: string | object, value?: any): this;
-        get(key: string, defaultValue?: any): any;
-    }
-
-    interface IDataManager {
-        set(key: string | object, value?: any): this;
-        get(key: string, defaultValue?: any): any;
-    }
-
-    // Panorama classes and interfaces
-    class panorama {
-        static Player: {
-            new(element: string | HTMLElement, point: string | number[], options?: PlayerOptions): Player;
-        };
-        static createPlayer(element: string | HTMLElement, point: string | number[], options?: PlayerOptions): Promise<Player>;
-        static locate(point: number[]): Promise<any[]>;
-        static isSupported(): boolean;
-    }
-
-    interface PlayerOptions {
-        direction?: number[];
-        span?: number[];
-        layer?: string;
-        controls?: string[];
-    }
-
-    interface Player {
-        events: IEventManager;
-        getPosition(): number[];
-        setPosition(point: number[]): Promise<void>;
-        getDirection(): number[];
-        setDirection(direction: number[]): void;
-        destroy(): void;
-    }
-
-    function ready(callback: Function): Promise<void>;
-    function geocode(request: string | number[], options?: any): Promise<any>;
-}
+  }
